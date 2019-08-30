dde5d2878d6cde058f9350c7c36f3c1fd4d40554
王杰 <wangjie-yf@log56.com>
2019-08-30 16:08:30 +0800

    配置调整

--- src/main/java/cn/wj/plugins/androidcodechecks/provider/CustomInspectionProvider.kt
+++ src/main/java/cn/wj/plugins/androidcodechecks/provider/CustomInspectionProvider.kt
@@ -1,23 +1,23 @@
-package cn.wj.plugins.androidcodechecks.provider
-
-import cn.wj.plugins.androidcodechecks.inspection.AndroidComponentNameJavaInspection
-import cn.wj.plugins.androidcodechecks.inspection.AndroidComponentNameKotlinInspection
-import com.intellij.codeInspection.InspectionToolProvider
-
-/**
- * 检查工具提供者
- */
-class CustomInspectionProvider : InspectionToolProvider {
-
-    companion object {
-        /** 检查工具列表 */
-        private val INSPECTIONS = arrayOf<Class<*>>(
-                AndroidComponentNameKotlinInspection::class.java,
-                AndroidComponentNameJavaInspection::class.java
-        )
-    }
-
-    override fun getInspectionClasses(): Array<Class<*>> {
-        return INSPECTIONS
-    }
-}
+//package cn.wj.plugins.androidcodechecks.provider
+//
+//import cn.wj.plugins.androidcodechecks.inspection.AndroidComponentNameJavaInspection
+//import cn.wj.plugins.androidcodechecks.inspection.AndroidComponentNameKotlinInspection
+//import com.intellij.codeInspection.InspectionToolProvider
+//
+///**
+// * 检查工具提供者
+// */
+//class CustomInspectionProvider : InspectionToolProvider {
+//
+//    companion object {
+//        /** 检查工具列表 */
+//        private val INSPECTIONS = arrayOf<Class<*>>(
+//                AndroidComponentNameKotlinInspection::class.java,
+//                AndroidComponentNameJavaInspection::class.java
+//        )
+//    }
+//
+//    override fun getInspectionClasses(): Array<Class<*>> {
+//        return INSPECTIONS
+//    }
+//}
